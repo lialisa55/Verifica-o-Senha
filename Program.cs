4314ffc8185364141c612f6ffc2820b87571c24e
unknown <nathalia.mota@etec.sp.gov.br>
2023-03-03 20:31:17 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VerificacaoSenha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "cachorro";
+            string userinput;
+
+            do
+            {
+                Console.WriteLine("Digite a senha");
+                userinput = Console.ReadLine();
+                Console.Clear();
+            } while (userinput != senha);
+
+            Console.WriteLine("Senha correta");
+        }
+    }
+}
